f17431ca6b93e3953c1f2b89768078080ccfef2a
VelamuriPrasad <72890831+VelamuriPrasad@users.noreply.github.com>
2020-10-15 15:47:19 +0530

    Update HelloWorld.java

--- Hello-Worlds/HelloWorld.java
+++ Hello-Worlds/HelloWorld.java
@@ -1,8 +1,8 @@
 import static java.lang.System.out;
 
-public class HelloWorld{
+public class HelloWorld1{
 
      public static void main(String []args){
-        out.println("Hello World");
+        System.out.println("Hello World");
      }
 }
